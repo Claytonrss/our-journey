7f17e0ecb3a0828c0f135e21199fae0c8fe3ba07
Clayton Rafael <clayton.rssouza@gmail.com>
2026-06-18 20:59:59 -0300

    chore: update Content-Security-Policy to allow external font sources and blob workers

--- next.config.ts
+++ next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' https://res.cloudinary.com data: blob:; connect-src 'self' https://api.mapbox.com https://events.mapbox.com https://api.spotify.com; font-src 'self'; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self';",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' https://res.cloudinary.com data: blob:; connect-src 'self' https://api.mapbox.com https://events.mapbox.com https://api.spotify.com; font-src 'self' data: https://fonts.gstatic.com; worker-src 'self' blob:; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self';",
           },
         ],
       },
